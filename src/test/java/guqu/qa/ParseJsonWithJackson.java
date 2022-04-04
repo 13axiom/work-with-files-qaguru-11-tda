37eff8677fd2c4cd2ee98e833372d2d6d4c70ad3
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-05 00:02:35 +0300

    ParseJsonWithJackson.java is added v.1.1

--- src/test/java/guqu/qa/ParseJsonWithJackson.java
+++ src/test/java/guqu/qa/ParseJsonWithJackson.java
@@ -1,14 +1,11 @@
 package guqu.qa;
 
-import com.google.gson.JsonObject;
 import guqu.qa.domain.QuestionsFormat;
 import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,23 +15,11 @@ public class ParseJsonWithJackson {
     @Test
     void jsonCommonTestByJackson() throws IOException {
         ObjectMapper om = new ObjectMapper();
-        QuestionsFormat json = om.readValue(Paths.get("src/test/resources/files/questions.json").toFile(), QuestionsFormat.class);
-        assertThat(json.quiz.maths.q2.answer).isEqualTo("4");
-        assertThat(json.quiz.sport.q1.options).contains("New York Bulls");
-
-    }
-
-    @Test
-    void jsonCommonTestByJackson2() throws IOException {
-        ObjectMapper om = new ObjectMapper();
         try (InputStream is = classLoader.getResourceAsStream("files/questions.json")) {
             QuestionsFormat json = om.readValue(is, QuestionsFormat.class);
             assertThat(json.quiz.maths.q2.answer).isEqualTo("4");
             assertThat(json.quiz.sport.q1.options).contains("New York Bulls");
-
         }
-
     }
-
 
 }
