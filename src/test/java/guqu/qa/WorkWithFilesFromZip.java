a7aa68eba13d33307b7e8c8be2895e1d0eb2db17
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-05 11:27:40 +0300

    WorkWithFilesFromZip.java

--- src/test/java/guqu/qa/WorkWithFilesFromZip.java
+++ src/test/java/guqu/qa/WorkWithFilesFromZip.java
@@ -1,20 +1,16 @@
 package guqu.qa;
 
-import com.google.gson.JsonObject;
-import guqu.qa.domain.QuestionsFormat;
-import io.qameta.allure.internal.shadowed.jackson.core.JsonParser;
-import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,50 +18,83 @@ public class WorkWithFilesFromZip {
 
     ClassLoader classLoader = getClass().getClassLoader();
 
-
-    /*@Test
+    @Test
     void parseFilesInZipTest() throws Exception {
-        try
-          (InputStream is = classLoader.getResourceAsStream("files/work-with-files-qaguru-11-tda-zip-pdf-csv-xlsx.zip");
-                ZipFile zp = new ZipFile(is)) {
-            Enumeration<? extends ZipEntry> entry = zp.entries();
-            while (entry.hasMoreElements()) {
-                ZipEntry ze = entry.nextElement();
-                System.out.println("Entry: "+ze.getName());
+        //создаем объект ZipFile (берем зип архив по ссылке)
+        ZipFile zp = new ZipFile("src/test/resources/files/work-with-files-qaguru-11-tda-zip-pdf-csv-xlsx.zip");
 
+        //далее получаем перечисление элементов оглавления архива методом entries
+        Enumeration<? extends ZipEntry> entry = zp.entries();
+
+        //пользуясь этим перечислением, организовываем извлечение всех элементов оглавления для последующей записи в массив
+        while (entry.hasMoreElements()) {
+            ZipEntry ze = entry.nextElement();
+
+            //исключаем извлечение служебных файлов MacOs
+            if (ze.getName().contains("__MACOSX/.")) {
+                continue;
+            } else //если это не служебный файл, то производим обработку/проверку файла в зависимости от типа
+            {
+
+                //xlsx
+                if (ze.getName().contains(".xlsx")) {
+                    InputStream is = zp.getInputStream(ze);
+                    XLS xlsx = new XLS(is);
+                    System.out.println("Entry: " + ze.getName());
+                    assertThat(xlsx.name).isEqualTo("");
+                    assertThat(xlsx.excel.getSheetAt(1)
+                            .getRow(1)
+                            .getCell(0)
+                            .getStringCellValue()).isEqualTo("Для отрезков до 1200 метров - рассчитывается исходя из вашего времени на 5 километров (авторы предлагают в своих таблицах время для результатов на 5 км от 15:30 до 30:00):");
+
+                    assertThat(xlsx.excel.getSheetAt(1)
+                            .getRow(1)
+                            .getCell(0)
+                            .getStringCellValue()).contains("Для отрезков до 1200 метров");
+
+                }
+
+                //pdf
+                if (ze.getName().contains(".pdf")) {
+                    InputStream is = zp.getInputStream(ze);
+                    PDF pdf = new PDF(is);
+                    System.out.println("Entry: " + ze.getName());
+                    assertThat(pdf.author).isEqualTo("");
+                    assertThat(pdf.numberOfPages).isEqualTo(4);
+                    assertThat(pdf.text).contains("Study Guide: Data Retrieval with SQL");
+                }
+
+                //csv
+                if (ze.getName().contains(".csv")) {
+                    try (InputStream is = zp.getInputStream(ze);
+                         CSVReader reader = new CSVReader(new InputStreamReader(is))) {
+                        List<String[]> content = reader.readAll();
+                        System.out.println("Entry: " + ze.getName());
+                        assertThat(content.get(0)).contains(
+                                "Series_reference",
+                                "Period",
+                                "Data_value",
+                                "Suppressed",
+                                "STATUS",
+                                "UNITS",
+                                "Magnitude",
+                                "Subject",
+                                "Group",
+                                "Series_title_1",
+                                "Series_title_2",
+                                "Series_title_3",
+                                "Series_title_4",
+                                "Series_title_5");
+
+                        assertThat(content.get(7)).contains(
+                                "36422");
+                    }
+                }
             }
         }
-    }*/
-    /*@Test
-    void jsonCommonTestByGson() throws Exception {
-        Gson gson = new Gson();
-        try (InputStream is = classLoader.getResourceAsStream("files/questions.json")) {
-            String json = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
-            assertThat(jsonObject.get("quiz").getAsJsonObject().get("maths").getAsJsonObject().get("q1").getAsJsonObject().get("answer").getAsString()).isEqualTo("12");
-            //assertThat(jsonObject.get("address").getAsJsonObject().get("street").getAsString()).isEqualTo("Mira");
-        }
-    }*/
-
-    @Test
-    void jsonCommonTestByJackson() throws IOException {
-        ObjectMapper om = new ObjectMapper();
-        QuestionsFormat json = om.readValue(Paths.get("src/test/resources/files/questions.json").toFile(), QuestionsFormat.class);
-        assertThat(json.quiz.maths.q2.answer).isEqualTo("4");
-        assertThat(json.quiz.sport.q1.options).contains("New York Bulls");
-
-        }
-
-
-   /* @Test
-    void jsonTypeTest() throws Exception {
-        Gson gson = new Gson();
-        try (InputStream is = classLoader.getResourceAsStream("files/questions.json")) {
-            String json = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            Teacher jsonObject = gson.fromJson(json, Teacher.class);
-            assertThat(jsonObject.name).isEqualTo("Dmitrii");
-            assertThat(jsonObject.address.street).isEqualTo("Mira");
-        }
-    }*/
+    }
 
 }
+
+
+
