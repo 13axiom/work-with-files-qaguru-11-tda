e2e9a1b79877d7f8ddd43d042e97e51dacb0ec1b
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-07 12:40:23 +0300

    ZoomSelenideTestAndNoBanner.java

--- src/test/java/guqu/qa/ZoomSelenideTestAndNoBanner.java
+++ src/test/java/guqu/qa/ZoomSelenideTestAndNoBanner.java
@@ -2,13 +2,16 @@ package guqu.qa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class ZoomSelenideTestAndNoBanner {
 
+
         @Test
+        @DisplayName("from lesson Selenide#2")
     public void zoomSelenideTestAndNoBanner() {
         open("https://demoqa.com/automation-practice-form");
             Selenide.zoom(0.75);
