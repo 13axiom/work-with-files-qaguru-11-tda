0fe46697c8fabb9bd1df24e133e8694736b030fc
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-07 12:39:45 +0300

    ZoomSelenideTestAndNoBanner.java

---
+++ src/test/java/guqu/qa/ZoomSelenideTestAndNoBanner.java
@@ -0,0 +1,20 @@
+package guqu.qa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class ZoomSelenideTestAndNoBanner {
+
+        @Test
+    public void zoomSelenideTestAndNoBanner() {
+        open("https://demoqa.com/automation-practice-form");
+            Selenide.zoom(0.75);
+            Configuration.browserSize = "1920x1080";
+            executeJavaScript("document.querySelector(\"footer\").hidden = 'true';" +
+                    "document.querySelector(\"#fixedban\").hidden = 'true'");
+        sleep(5000);
+    }
+}
